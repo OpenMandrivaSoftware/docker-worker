8b26c4073b1c7ac72cebae5840665944a6fe53ae
Wedge <shadowpilot34@gmail.com>
2016-01-16 12:09:49 +0300

    tracking bugs #2

--- lib/abf-worker/inspectors/live_inspector.rb
+++ lib/abf-worker/inspectors/live_inspector.rb
@@ -16,6 +16,8 @@ module AbfWorker::Inspectors
             sleep CHECK_INTERVAL
             stop_build if kill_now?
           rescue => e
+            puts e.message
+            puts e.backtrace
           end
         end
       end
